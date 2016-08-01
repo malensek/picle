3ccb103f006ee8e06ebfdff1857234f5615323f4
Matthew Malensek <matt@malensek.net>
2016-07-31 23:07:53 -0600

    Config file formatting

--- config/main.sh
+++ config/main.sh
@@ -2,13 +2,19 @@
 # Sample master configuration file. Must be located in ${PICLE_CONF}, which
 # defaults to ${PICLE_HOME}/config.
 ################################################################################
-MASTER_NODE="jr-0"
-CLUSTER_MEMBERS="${PICLE_HOME}/config/cluster-members.txt"
-USER_HOME_DIR="/srv/nfs/${MASTER_NODE}/home"
-UPDATE_DIR="/srv/nfs/${MASTER_NODE}/sys/updates"
 
-NFS_DIRS="/srv/nfs/${MASTER_NODE}/
-          /srv/nfs/${MASTER_NODE}/home
-          /srv/nfs/${MASTER_NODE}/sys
+# Node Configuration #
+PRIMARY_NODE="jr-0"
+CLUSTER_MEMBERS="${PICLE_HOME}/config/cluster-members.txt"
+UPDATE_DIR="/srv/nfs/${PRIMARY_NODE}/sys/updates"
+
+# Users #
+USER_GROUPS="users" # (comma-separated)
+USER_HOME_DIR="/srv/nfs/${PRIMARY_NODE}/home"
+
+# NFS #
+NFS_DIRS="/srv/nfs/${PRIMARY_NODE}/
+          /srv/nfs/${PRIMARY_NODE}/home
+          /srv/nfs/${PRIMARY_NODE}/sys
 "
 NFS_OPTS="rw,no_subtree_check,nohide,root_squash"
