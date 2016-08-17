a8b12ebcbb8c17be3fc94620898285780e9569b5
Matthew Malensek <matt@malensek.net>
2016-08-17 11:50:19 -0600

    Update shared functions

---
+++ shared/functions.sh
@@ -0,0 +1,23 @@
+
+echo_info() {
+    echo_color 34 "[>] ${@}"
+}
+
+echo_info_delay() {
+    echo_info "${@}"
+    sleep 1
+}
+
+echo_color() {
+    color="${1}"
+    shift 1
+    echo -e "\e[0;${color}m${@}\e[0m"
+}
+
+load_config() {
+    if [[ -z "${PICLE_CONF}" ]]; then
+        PICLE_CONF="${PICLE_HOME}/config/config.sh"
+    fi
+    source "${PICLE_CONF}" \
+        || die "Could not load configuration: ${PICLE_CONF}"
+}
