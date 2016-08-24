a4e7f617bf9a4dd6a5556263df58aa8dc16587fb
Matthew Malensek <matt@malensek.net>
2016-08-24 12:36:12 -0600

    Add doc comments

--- config/main.sh
+++ config/main.sh
@@ -1,3 +1,7 @@
+################################################################################
+# Sample master configuration file. Must be located in ${PICLE_CONF}, which
+# defaults to ${PICLE_HOME}/config.
+################################################################################
 MASTER_NODE="pi-0"
 CLUSTER_DEF="${PICLE_HOME}/pi-cluster.txt"
 USER_HOME_DIR="/srv/nfs/${MASTER_NODE}/home"
