c7add2de6efcce4b5b5fa656e9f589db9787879b
Matthew Malensek <matt@malensek.net>
2016-09-01 10:56:45 -0600

    Add NFS configuration example

--- config/main.sh
+++ config/main.sh
@@ -6,3 +6,9 @@ MASTER_NODE="pi-0"
 CLUSTER_DEF="${PICLE_HOME}/pi-cluster.txt"
 USER_HOME_DIR="/srv/nfs/${MASTER_NODE}/home"
 UPDATE_DIR="/srv/nfs/${MASTER_NODE}/sys/updates"
+
+NFS_DIRS="/srv/nfs/${MASTER_NODE}/
+          /srv/nfs/${MASTER_NODE}/home
+          /srv/nfs/${MASTER_NODE}/sys
+"
+NFS_OPTS="rw,no_subtree_check,nohide,root_squash"
